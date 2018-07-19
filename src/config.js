0b9107171213c778fe53064f2220d46ea6b4ca4f
Paul Galow <>
2018-07-19 22:02:59 +0200

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,51 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+// Configuration file for our app that will reference all the resources we have created
+
+// Dev stage configuration
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "eu-central-1",
+    // Bucket to store our user's uploads
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-cp65mmu43t22"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "eu-central-1",
+    URL: "https://c38rqc59j8.execute-api.eu-central-1.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_BJrY7XJWq",
+    APP_CLIENT_ID: "2sq47h30nt1okkff3m5j4kk20k",
+    IDENTITY_POOL_ID: "eu-central-1:d2220d73-5046-473c-929b-f37411de9483"
   }
+};
+
+// Prod stage configuration
+const prod = {
+  s3: {
+    REGION: "eu-central-1",
+    // Bucket to store our user's uploads
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-11zgowrvch986"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://wrisy9fo86.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_VtNkM4sat",
+    APP_CLIENT_ID: "1ih5lsfqkmcr05nk4nr7d2e07q",
+    IDENTITY_POOL_ID: "eu-central-1:2af5a043-d54b-4c03-8cb0-b29040f6a156"
+  }
+};
+
+// Default to 'dev' if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+// Add common config values here
+export default {
+  // Maximum size for user file attachments in byte
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
