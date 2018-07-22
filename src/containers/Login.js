16f9fc90a80dc11da6ed03eaeb2fb98e6a5e2d71
Paul Galow <>
2018-07-22 19:17:06 +0200

    Add comments to improve documentation

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -8,6 +8,7 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
 
+    // Creating a state object. This will be where we’ll store what the user enters in the form.
     this.state = {
       isLoading: false,
       email: "",
@@ -15,23 +16,32 @@ export default class Login extends Component {
     };
   }
 
+  // Validate user input in forms
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
+  // Handle updating the state when the user types something into the fields
+  // To have access to the 'this' keyword inside handleChange we store the reference
+  // to an anonymous function: handleChange = event => { }
   handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
     });
   }
 
+  // Will be triggered when login form is submitted
   handleSubmit = async event => {
+    // Suppress browser default behavior
     event.preventDefault();
-
+    // Set isLoading to true to use this property to display user feedback
     this.setState({ isLoading: true });
 
     try {
+      // Call aws-amplify's Auth method to sign in to Cognito
+      // Returns a promise since it will be logging the user asynchronously
       await Auth.signIn(this.state.email, this.state.password);
+      // Update isAuthenticated flag
       this.props.userHasAuthenticated(true);
     } catch (e) {
       alert(e.message);
@@ -46,6 +56,8 @@ export default class Login extends Component {
           <FormGroup controlId="email" bsSize="large">
             <ControlLabel>Email</ControlLabel>
             <FormControl
+              // Set autoFocus flag for email field, so that when our 
+              // form loads, it sets focus to this field.                           
               autoFocus
               type="email"
               value={this.state.email}
@@ -61,6 +73,7 @@ export default class Login extends Component {
             />
           </FormGroup>
           <LoaderButton
+            // Span button width to full width parent
             block
             bsSize="large"
             disabled={!this.validateForm()}
