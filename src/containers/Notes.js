20f0e307baf38c95d2429f6952faec373f730684
Paul Galow <>
2018-07-22 20:35:29 +0200

    Add comments to improve documentation

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -24,9 +24,12 @@ export default class Notes extends Component {
   async componentDidMount() {
     try {
       let attachmentURL;
+      // Load the note by calling getNote()
       const note = await this.getNote();
       const { content, attachment } = note;
 
+      // If there is an attachment, we use the key to get a secure link to the file
+      // we uploaded to S3. We then store this to the component's state as attachmentURL
       if (attachment) {
         attachmentURL = await Storage.vault.get(attachment);
       }
@@ -41,16 +44,22 @@ export default class Notes extends Component {
     }
   }
 
+  // Handles loading of notes from DynamoDB. We get the note id from the URL using 
+  // the props automatically passed to us by React-Router in 'this.props.match.params.id'.
+  // They keyword 'id' is a part of the pattern matching in our route (/notes/:id).
   getNote() {
     return API.get("notes", `/notes/${this.props.match.params.id}`);
   }
 
+  // Using AWS Amplify make a PUT request with the note object to /notes/:id where we get 
+  // the id from 'this.props.match.params.id'.
   saveNote(note) {
     return API.put("notes", `/notes/${this.props.match.params.id}`, {
       body: note
     });
   }
 
+  // Using AWS Amplify to make a DELETE request to /notes/:id which calls our delete API
   deleteNote() {
     return API.del("notes", `/notes/${this.props.match.params.id}`);
   }
@@ -59,6 +68,8 @@ export default class Notes extends Component {
     return this.state.content.length > 0;
   }
 
+  // We format the attachment URL using formatFilename by stripping the timestamp we 
+  // had added to the filename while uploading it.
   formatFilename(str) {
     return str.replace(/^\w+-/, "");
   }
@@ -86,14 +97,20 @@ export default class Notes extends Component {
     this.setState({ isLoading: true });
 
     try {
+      // If there is a file to upload we call s3Upload to upload it and save 
+      // the key we get from S3.
       if (this.file) {
         attachment = await s3Upload(this.file);
       }
 
+      // Save note by calling saveNote()
       await this.saveNote({
         content: this.state.content,
+        // Save attachment key of newly uploaded file or take the key of an 
+        // already uploaded attachment
         attachment: attachment || this.state.note.attachment
       });
+      // On success redirect the user to the home page
       this.props.history.push("/");
     } catch (e) {
       alert(e);
@@ -104,6 +121,7 @@ export default class Notes extends Component {
   handleDelete = async event => {
     event.preventDefault();
 
+    // Asks user to confirm if they want to delete the note
     const confirmed = window.confirm(
       "Are you sure you want to delete this note?"
     );
@@ -115,7 +133,9 @@ export default class Notes extends Component {
     this.setState({ isDeleting: true });
 
     try {
+      // Delete note
       await this.deleteNote();
+      // await s3Delete(this.state.note.attachment);
       this.props.history.push("/");
     } catch (e) {
       alert(e);
@@ -126,6 +146,7 @@ export default class Notes extends Component {
   render() {
     return (
       <div className="Notes">
+        {/* We render our form only when this.state.note is available */}
         {this.state.note &&
           <form onSubmit={this.handleSubmit}>
             <FormGroup controlId="content">
@@ -135,6 +156,7 @@ export default class Notes extends Component {
                 componentClass="textarea"
               />
             </FormGroup>
+            {/* Display attachment block if attachment is available */}
             {this.state.note.attachment &&
               <FormGroup>
                 <ControlLabel>Attachment</ControlLabel>
@@ -153,6 +175,7 @@ export default class Notes extends Component {
                 <ControlLabel>Attachment</ControlLabel>}
               <FormControl onChange={this.handleFileChange} type="file" />
             </FormGroup>
+            {/* Save button */}
             <LoaderButton
               block
               bsStyle="primary"
@@ -163,6 +186,7 @@ export default class Notes extends Component {
               text="Save"
               loadingText="Saving…"
             />
+            {/* Delete button */}
             <LoaderButton
               block
               bsStyle="danger"
